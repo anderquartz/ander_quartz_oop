cb08997068960048fd13c4d8fe264f4fb06e27a4
anderquartz <iguarik25@gmail.com>
2026-02-24 19:27:55 +0300

    make exercise 1

--- less_3/homework_3.py
+++ less_3/homework_3.py
@@ -1,1 +1,20 @@
-print()
+# 1. Создай класс Circle, в котором:
+# есть атрибуты класса MIN_RADIUS = 1 и MAX_RADIUS = 1000,
+# метод класса is_valid_radius(cls, r), который проверяет, входит ли значение в допустимый диапазон.
+# Проверь результат вызова:
+# print(Circle.is_valid_radius(500))   # True
+# print(Circle.is_valid_radius(1500))  # False
+
+
+
+class Circle:
+    MIN_RADIUS = 1
+    MAX_RADIUS = 1000
+
+    @classmethod
+    def is_valid_radius(cls, r):
+        return cls.MIN_RADIUS <= r <= cls.MAX_RADIUS
+
+print(Circle.is_valid_radius(500))
+print(Circle.is_valid_radius(1500))
+
