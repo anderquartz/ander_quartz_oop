e9c01e8179808634c958cbd55b0d5236ff43d146
anderquartz <iguarik25@gmail.com>
2026-02-24 19:35:55 +0300

    make exercise 2

--- less_3/homework_3.py
+++ less_3/homework_3.py
@@ -5,16 +5,40 @@
 # print(Circle.is_valid_radius(500))   # True
 # print(Circle.is_valid_radius(1500))  # False
 
+# 2. Добавь в класс Circle:
+# статический метод area(radius),
+# который возвращает площадь круга по формуле π * r ** 2 (используй импорт math.pi),
+# инициализацию в __init__, которая сохраняет радиус,
+# только если он проходит валидацию через метод is_valid_radius()
+# (подумай как можно проверить значения перед тем как записать их в переменные экземпляра класса)
+# Пример:
+# c = Circle(10)
+# print(c.area(c.radius))  # Площадь круга
 
+
+
+from math import pi
 
 class Circle:
     MIN_RADIUS = 1
     MAX_RADIUS = 1000
 
+    def __init__(self, radius):
+        if self.is_valid_radius(radius):
+            self.radius = radius
+
     @classmethod
     def is_valid_radius(cls, r):
         return cls.MIN_RADIUS <= r <= cls.MAX_RADIUS
 
+    @staticmethod
+    def area(radius):
+        return pi * radius ** 2
+
 print(Circle.is_valid_radius(500))
 print(Circle.is_valid_radius(1500))
 
+print()
+
+c = Circle(10)
+print(c.area(c.radius))
